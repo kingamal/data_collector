e24b725a83e23e0aa3871daffddabf7f9d2d3ed7
Kinga <kinga.maleszewska@o2.pl>
2024-11-27 13:52:42 +0100

    Creating the postgresql db model and storing user data to it

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('SQLALCHEMY_DATABASE_URI')
+db=SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__="data"
+    id=db.Column(db.Integer, primary_key=True)
+    email_=db.Column(db.String(120), unique=True)
+    height_=db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_=email_
+        self.height_=height_
+
 
 @app.route("/")
 def index():
@@ -11,7 +29,13 @@ def success():
     if request.method=="POST":
         email=request.form["email_name"]
         height=request.form["height_name"]
-    return render_template("success.html")
+        if db.session.query(Data).filter(Data.email_==email).count() == 0:
+            data=Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+    return render_template("index.html", 
+        text="Seems like we've got something from that email address already")
 
 if __name__ == "__main__":
     app.debug=True
