04286cad0574d9fdd4e5787e6e64090d5211bfac
Kinga <kinga.maleszewska@o2.pl>
2024-11-28 09:35:39 +0100

    Creating a download-upload feature with sample file

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 from dotenv import load_dotenv
 import os
 from send_email import send_email
+from werkzeug.utils import secure_filename
 
 load_dotenv()
 
@@ -24,24 +25,35 @@ class Data(db.Model):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", btn=None)
 
 @app.route("/success", methods=["POST"])
 def success():
+    global file
     if request.method=="POST":
-        email=request.form["email_name"]
-        height=request.form["height_name"]
-        if db.session.query(Data).filter(Data.email_==email).count() == 0:
-            data=Data(email, height)
-            db.session.add(data)
-            db.session.commit()
-            average_height=db.session.query(func.avg(Data.height_)).scalar()
-            average_height=round(average_height,1)
-            count=db.session.query(Data.height_).count()
-            send_email(email, height, average_height, count)
-            return render_template("success.html")
-    return render_template("index.html", 
-        text="Seems like we've got something from that email address already")
+        # email=request.form["email_name"]
+        # height=request.form["height_name"]
+        file=request.files["file"]
+        filename="uploaded"+file.filename
+        file.save(secure_filename(filename))
+        with open(filename, "a") as f:
+            f.write("\nThis was added later!")
+        # if db.session.query(Data).filter(Data.email_==email).count() == 0:
+        #     data=Data(email, height)
+        #     db.session.add(data)
+        #     db.session.commit()
+        #     average_height=db.session.query(func.avg(Data.height_)).scalar()
+        #     average_height=round(average_height,1)
+        #     count=db.session.query(Data.height_).count()
+        #     send_email(email, height, average_height, count)
+        #     return render_template("success.html")
+    return render_template("index.html", btn="download.html")
+    # return render_template("index.html", 
+    #     text="Seems like we've got something from that email address already")
+
+@app.route("/download")
+def download():
+    return send_file("uploaded"+file.filename, download_name="yourfile.csv", as_attachment=True)
 
 if __name__ == "__main__":
     app.debug=True
